79a9b8a425f2e702b978a879d80319439c7034f8
Ian <m.iangreene@gmail.com>
2018-09-23 19:31:10 -0400

    Day 007 of 100DoCC

--- app/src/main/java/co/codemaestro/twoactivities/MainActivity.java
+++ app/src/main/java/co/codemaestro/twoactivities/MainActivity.java
@@ -8,10 +8,8 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG =
-            MainActivity.class.getSimpleName();
-    public static final String EXTRA_MESSAGE =
-            "co.codemaestro.twoactivities.extra.MESSAGE";
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "co.codemaestro.twoactivities.extra.MESSAGE";
     private EditText mMessageEditText;
 
     @Override
